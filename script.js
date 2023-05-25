56a27959d6e219304e10c30796e3e9cf01bd1104
Gabriel Dos Santos Monteiro <bhcmonteiro54@gmail.com>
2023-05-24 22:47:56 -0300

    HTML e CSS prontos

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function selecionarItens(selecao) {
-    const selecionado = window.document.querySelector('.prato');
 
-    if (selecionado != null) {
-        selecionado.classList.toggle('selecionado');
-    } else {
-        selecionado.classList.remove('selecionado');
-    }
-}
