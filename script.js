b53d7dc6bea658f943d0b70616597b973bbd75f1
Gabriel Dos Santos Monteiro <bhcmonteiro54@gmail.com>
2023-05-27 00:03:57 -0300

    Requisitos de habilitar botão e do ícones prontos

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-// Criar uma função para selecionar o prato
-function selecionarPrato(seletor) {
+let prato;
+let bebida;
+let sobremesa;
+
+function selecionarBotao() {
+    console.log(prato);
+    console.log(bebida);
+    console.log(sobremesa);
+
+    // Verficar se o prato foi selecionado
+    if (prato !== undefined) {
+        // Verificar se a bebida foi selecionada
+        if (bebida !== undefined) {
+            // Verificar se a sobremesa foi selecionada
+            if (sobremesa !== undefined) {
+
+                const botaoPedido = window.document.querySelector('.botao');
+                botaoPedido.classList.add('ativarBotao');
+                botaoPedido.innerHTML = 'Fechar Pedido';
+                botaoPedido.removeAttribute('disabled');
+
+            }
+
+        }
+    }
+}
+
+// Criar uma função para selecionar o prato (usando função parametrizada)
+
+/*function selecionarPrato(seletor) {
 
     // Verificar se já existe um prato com a borda e buscar esse prato usando QuerySlector
-    const pratoAnterior = window.document.querySelector('.menu-prato .borda'); 
+    const pratoAnterior = window.document.querySelector('.menu-prato .borda');
     console.log(pratoAnterior);
 
     // Se já existir um prato com a borda, vou remover a borda desse prato
@@ -10,44 +38,66 @@ function selecionarPrato(seletor) {
         pratoAnterior.classList.remove('borda');
     }
 
-    // Selecionar o prato que vai ficar com a borda verde
-    const selecionado = window.document.querySelector(seletor);
+    // Selecionar agora o novo prato que vai ficar com a borda verde, buscar o novo prato
+    const pratoSelecionado = window.document.querySelector(seletor);
+
     // Adiconar a borda verde nesse prato
-    selecionado.classList.add('borda');
+    pratoSelecionado.classList.add('borda');
+}*/
+
+//(usando o This)
+function selecionarPrato(pratoSelecionado) {
+
+    // Verificar se já existe um prato com a borda e buscar esse prato usando QuerySlector
+    const pratoAnterior = window.document.querySelector('.menu-prato .borda');
+
+    // Se já existir um prato com a borda, vou remover a borda desse prato
+    if (pratoAnterior != null) {
+        pratoAnterior.classList.remove('borda');
+    }
+
+    // Adiconar a borda verde nesse prato
+    pratoSelecionado.classList.add('borda');
+
+    prato = pratoSelecionado.querySelector('.titulo-prato').innerHTML;
+
+    selecionarBotao();
 }
 
 // Criar uma função para selecionar a bebida
-function selecionarBebida(seletor) {
+function selecionarBebida(bebidaSelecionada) {
 
     // Verificar se já existe uma bebida com a borda e buscar essa bebida usando QuerySlector
     const bebidaAnterior = window.document.querySelector('.menu-bebida .borda');
-    console.log(bebidaAnterior);
 
     // Se já existir uma bebida com a borda, vou remover a borda dessa bebida
     if (bebidaAnterior != null) {
         bebidaAnterior.classList.remove('borda');
     }
 
-    // Selecionar a bebida que vai ficar com a borda verde
-    const selecionado = window.document.querySelector(seletor);
     // Adiconar a borda verde nessa bebida
-    selecionado.classList.add('borda');
+    bebidaSelecionada.classList.add('borda');
+
+    bebida = bebidaSelecionada.querySelector('.titulo-bebida').innerHTML;
+
+    selecionarBotao();
 }
 
 // Criar uma função para selecionar a sobremesa
-function selecionarSobremesa(seletor) {
+function selecionarSobremesa(sobremesaSelecionada) {
 
     // Verificar se já existe uma sobremesa com a borda e buscar essa sobremesa usando QuerySlector
     const sobremesaAnterior = window.document.querySelector('.menu-sobremesa .borda');
-    console.log( sobremesaAnterior);
 
     // Se já existir uma sobremesa com a borda, vou remover a borda dessa sobremesa
-    if ( sobremesaAnterior != null) {
+    if (sobremesaAnterior != null) {
         sobremesaAnterior.classList.remove('borda');
     }
 
-    // Selecionar a sobremesa que vai ficar com a borda verde
-    const selecionado = window.document.querySelector(seletor);
     // Adiconar a borda verde nessa sobremesa
-    selecionado.classList.add('borda');
+    sobremesaSelecionada.classList.add('borda');
+
+    sobremesa = sobremesaSelecionada.querySelector('.titulo-sobremesa').innerHTML;
+
+    selecionarBotao();
 }
